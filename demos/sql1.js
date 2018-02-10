079c4bc0665d7e92f0cd0a6a594d79ad3975ff38
feixiangsnail <feixiangsnail@hotmail.com>
2018-02-10 11:10:50 +0800

    mysql

---
+++ demos/sql1.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var mysql = require('mysql');
+module.exports= function () {
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        port: '3306',
+        database: 'sitepoint'
+    });
+    con.connect((err) => {
+        if (err) {
+            console.log(err, 'err');
+            return;
+        }
+        console.log('connection established')
+    })
+    // con.query('SELECT 1 +1 AS solution', function (err, rows, fields) {
+    //     if (err) {
+    //         console.log(err, 'queryErr');
+    //         return;
+    //     }
+    //     console.log('solution',rows[0].solution);
+    // })
+    // con.end((err)=>{
+    //     if(err) return;
+    //     console.log('connection succeed')
+    // })
+    var addVip = 'insert into testutf8(username,password) values(?,?)';
+    var param = ['用汉字试试4', 4];
+    // con.query(addVip, param, function(error, result){
+    //     if(error)
+    //     {
+    //         console.log(error.message);
+    //     }else{
+    //         console.log('insert id: '+result.insertId);
+    //     }
+    // });
+    //删除一条数据
+    // con.query('delete from testutf8 where password = 3',(err,result)=>{
+    //     if(err){
+    //         console.log(err.message);
+    //     }else{
+    //         console.log(result.affectedRows);
+    //     }
+    // })
+    //修改一条数据
+    // var userSql = "update testutf8 set username = 'newUserName' where password = 2";
+    // con.query(userSql,(err,result)=>{
+    //     if(err){
+    //         console.log(err.message);
+    //     }else{
+    //         console.log(result.affectedRows);
+    //     }
+    // })
+    //查找数据
+    //var DATABASE = "node";
+    //con.query('use '+DATABASE);
+    con.query('select * from testutf8', (error, results, fields) => {
+        if (error) {
+            throw error;
+        }
+        if (results) {
+            for (var i = 0; i < results.length; i++) {
+                console.log(results[i].username);
+                console.log(results[i].password)
+            }
+            // console.log(results,'result');
+        }
+    })
+    con.end();
+
+}
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//var handleSql = require('./demos/sql1')();
 var express = require('express');
 var mysql = require('mysql');
 var con = mysql.createConnection({
@@ -5,77 +6,58 @@ var con = mysql.createConnection({
     user: 'root',
     password: '123456',
     port: '3306',
-    database: 'sitepoint'
+    
 });
-con.connect((err) => {
-    if (err) {
-        console.log(err, 'err');
-        return;
+con.connect((err)=>{
+    if(err){
+        console.log(err,'err');
     }
-    console.log('connection established')
+    else{
+        console.log('connected')
+    }
 })
 
-// con.query('SELECT 1 +1 AS solution', function (err, rows, fields) {
-//     if (err) {
-//         console.log(err, 'queryErr');
+// con.query('create database nodeSql',function(err){
+//     if(err){
+//         console.log(err,'err');
 //         return;
 //     }
-//     console.log('solution',rows[0].solution);
 // })
+//con.query('drop database nodeSql');
+//使用指定数据库
+    con.query('use sitepoint')
+//显示数据库中的所有数据表
+    // con.query('show tables',function(err,tables){
+    //     if(err){
+    //         console.log(err,'err')
 
-// con.end((err)=>{
-//     if(err) return;
-//     console.log('connection succeed')
-// })
-
-var addVip = 'insert into testutf8(username,password) values(?,?)';
-var param = ['用汉字试试4',4];
-// con.query(addVip, param, function(error, result){
-//     if(error)
-//     {
-//         console.log(error.message);
-//     }else{
-//         console.log('insert id: '+result.insertId);
-//     }
-// });
-
-//删除一条数据
-// con.query('delete from testutf8 where password = 3',(err,result)=>{
-//     if(err){
-//         console.log(err.message);
-
-//     }else{
-//         console.log(result.affectedRows);
-//     }
-// })
-//修改一条数据
-// var userSql = "update testutf8 set username = 'newUserName' where password = 2";
-
-// con.query(userSql,(err,result)=>{
-//     if(err){
-//         console.log(err.message);
-
-//     }else{
-//         console.log(result.affectedRows);
-//     }
-// })
-
-//查找数据
-//var DATABASE = "node";
-//connection.query('use '+DATABASE);
-con.query('select * from testutf8',(error,results,fields)=>{
-    if (error) {
-        throw error;
-    }
-    if(results){
-        for(var i = 0;i<results.length;i++){
-            console.log(results[i].username);
-            console.log(results[i].password)
-        }
-        // console.log(results,'result');
+    //     }else{
+    //         console.log(tables);
+    //     }
+    // })
+//为指定表增加字段
+    // con.query('alter table testutf8 add id int(15)')
+//为指定表更改字段名字段类型
+    //con.query('alter table testutf8 change usernameusernameusernameusername2 username varchar(20)')
+//为指定表删除字段
+    //con.query('alter table testutf8 drop id')
+//数据的增删改查
+var testutf8 ={
+    insert:'insert into testutf8(username,password) values(?,?)',
+    delete:'delete from testutf8 where password = 3',
+    update:"update testutf8 set username = 'newUserName' where password = 2",
+    queryById:'select * from testutf8 where username=? and password = ?',
+    queryAll:'select * from testutf8' //回调函数的第二个参数是所有行数组
+}
+con.query(testutf8.insert,['zhangxiaosan',666666],(err,result)=>{
+    if(err){
+        console.log(err,'err');
+    }else{
+        console.log(result,'result');
     }
 })
 
+con.query('alter table testutf8 add id int(10)')
 
 
 
@@ -93,7 +75,21 @@ con.query('select * from testutf8',(error,results,fields)=>{
 
 
 
-con.end();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
